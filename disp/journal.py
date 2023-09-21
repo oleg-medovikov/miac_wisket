5671c3f155321f443343c4d92a7e9bae2a0044b4
oleg-medovikov <oleg.medovikov@mail.ru>
2023-09-21 11:17:50 +0300

    add dolg to svo svod

--- disp/journal.py
+++ disp/journal.py
@@ -35,24 +35,6 @@ async def get_journal_commands(message: types.Message):
     return await message.answer(MESS, parse_mode='Markdown')
 
 
-@dp.message_handler(commands=['journal_time_stop_mounth'])
-async def journal_time_stop_mounth_manual(message: types.Message):
-    await delete_message(message)
-
-    USER = await User.get(message['from']['id'])
-    if USER is None:
-        return None
-    if not USER.admin:
-        return await message.answer(
-            "функция для админов",
-            parse_mode='Markdown'
-            )
-
-    await get_time_stop_mounth()
-
-    return await message.answer('Done', parse_mode='Markdown')
-
-
 @dp.message_handler(commands=['journal_time_start_mounth'])
 async def journal_time_start_mounth_manual(message: types.Message):
     await delete_message(message)
