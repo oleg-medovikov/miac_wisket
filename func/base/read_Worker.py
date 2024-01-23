3db1b7984db417da8110317101ff417035876db4
oleg_medovikov <oleg.medovikov@mail.ru>
2024-01-23 12:10:17 +0300

    bugs worker read excel

--- func/base/read_Worker.py
+++ func/base/read_Worker.py
@@ -1,4 +1,5 @@
 from pandas import read_excel
+from pandas._libs.tslibs.timestamps import Timestamp
 from sqlalchemy import and_
 from datetime import datetime
 from ast import literal_eval
@@ -8,7 +9,10 @@ from mdls import User, Worker
 
 async def read_Worker(user: User) -> str:
     df = read_excel(
-        "/tmp/_Worker.xlsx", usecols=[_.name for _ in Worker.__table__.columns]
+        "/tmp/_Worker.xlsx",
+        usecols=[
+            _.name for _ in Worker.__table__.columns if _.name not in ["date_update"]
+        ],
     )
 
     mess = ""
@@ -18,14 +22,25 @@ async def read_Worker(user: User) -> str:
                 row[key] = None
 
         for key in ["id_svup"]:
-            if not isinstance(row[key], str):
+            if isinstance(row[key], str):
                 row[key] = [int(_) for _ in literal_eval(row[key])]
 
         for key in ["name", "first_name", "oid", "mid_name", "phone"]:
             if isinstance(row[key], str):
                 row[key] = row[key].replace("\u2028", "\n")
+            elif isinstance(row[key], int):
+                row[key] = str(row[key])
             else:
                 row[key] = ""
+
+        for key in ["birthday"]:
+            if isinstance(row[key], str):
+                try:
+                    row[key] = datetime.strptime(row[key], "%d.%m.%Y").date()
+                except:
+                    row[key] = None
+            elif isinstance(row[key], Timestamp):
+                row[key] = row[key].date()
 
         # если есть идентичная строчка пропускаем
         object = await Worker.query.where(
@@ -36,9 +51,11 @@ async def read_Worker(user: User) -> str:
             continue
         # есть есть строчка с таким же id - апдейтим
         object = await Worker.query.where(Worker.id == row["id"]).gino.first()
+
+        row["u_id"] = user.id
+        row["date_update"] = datetime.now()
+
         if object is not None:
-            row["u_id"] = user.id
-            row["date_update"] = datetime.now()
             await object.update(
                 **{
                     key: value
@@ -49,7 +66,6 @@ async def read_Worker(user: User) -> str:
             mess += f"\nОбновил строку: {row['name']}"
             continue
         # или создаем новую строку
-        row["u_id"] = user.id
         await Worker.create(
             **{
                 key: value
